bfbb602343a80e5edf0674a8231931efcc9f145e
05ankit <121804668+05ankit@users.noreply.github.com>
2024-07-29 23:41:11 +0530

    Add files via upload

---
+++ DAY13/ChoclateDistribution.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{ 
+public:
+    long long f(vector<long long> a, long long l, long long r, long long &ans, long long m){
+    if (l >= r) return LONG_MAX;
+    if (r - l + 1 < m) return LONG_MAX;
+    ans = min(ans, a[r] - a[l]); // Update ans with the current minimum difference
+    f(a, l + 1, r, ans, m);
+    f(a, l, r - 1, ans, m);
+    return a[r] - a[l];
+    
+    }
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+    //code
+         long long ans=LONG_MAX;
+         sort(a.begin(),a.end());
+         ans=a[n-1]-a[0];
+         f(a,0,n-1,ans, m);
+         return ans;
+    }   
+};
